97ae5fe8a36abacb47258fcf12493cd7efd610f4
David <dvddacosta47@gmail.com>
2026-03-27 11:02:27 +0000

    Refactor script.js to comment out previous code examples and retain shutdown function

--- practice-1/script.js
+++ practice-1/script.js
@@ -1,32 +1,44 @@
-const person = {
-    name: "David",
-    age: "31",
-    profession: "programmer",
-};
+// const person = {
+//     name: "David",
+//     age: "31",
+//     profession: "programmer",
+// };
 
-const { name, age, profession } = person;
-console.log(`My name is ${name}, I'm ${age} years old and I work as a ${profession}.`);
+// const { name, age, profession } = person;
+// console.log(`My name is ${name}, I'm ${age} years old and I work as a ${profession}.`);
 
-const nums = [1, 2, 3, 4, 5];
-nums.shift();
+// const nums = [1, 2, 3, 4, 5];
+// nums.shift();
 
-console.log(nums);
+// console.log(nums);
 
-nums.push(6);
+// nums.push(6);
 
-console.log(nums);
+// console.log(nums);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function filterEvenNumbers(numbers) {
-    const evenNumbers = [];
-    for (let i = 0; i < numbers.length; i++) {
-        const currentNumber = numbers[i];
-        if (currentNumber % 2 !== 0) {
-            evenNumbers.push(currentNumber);
-        }   
-    };
-    return evenNumbers;
-};
+// function filterEvenNumbers(numbers) {
+//     const evenNumbers = [];
+//     for (let i = 0; i < numbers.length; i++) {
+//         const currentNumber = numbers[i];
+//         if (currentNumber % 2 !== 0) {
+//             evenNumbers.push(currentNumber);
+//         }   
+//     };
+//     return evenNumbers;
+// };
 
-console.log(filterEvenNumbers(numbers));
+// console.log(filterEvenNumbers(numbers));
+
+function shutDown(callback) {
+    console.log("Shutdown initiated...");
+    setTimeout(() => {
+        console.log("Shutdown complete.");
+        callback();
+    }, 2000);
+}
+
+shutDown(() => {
+    console.log("Callback executed after shutdown.");
+});
